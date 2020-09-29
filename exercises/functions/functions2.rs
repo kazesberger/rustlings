786a17b4275957c1a4ba3ae80142621ec83e681a
kazesberger <klaus.azesberger@bearingpoint.com>
2020-09-29 23:41:30 +0200

    Add 100days-of-rust day-3

--- exercises/functions/functions2.rs
+++ exercises/functions/functions2.rs
@@ -1,14 +1,13 @@
 // functions2.rs
 // Make me compile! Execute `rustlings hint functions2` for hints :)
 
-// I AM NOT DONE
 
 fn main() {
-    call_me(3);
+    call_me(30);
 }
 
-fn call_me(num) {
-    for i in 0..num {
+fn call_me(num: u8) {
+    for i in &[1,2,3,4] {
         println!("Ring! Call number {}", i + 1);
     }
 }
--- exercises/structs/structs3.rs
+++ exercises/structs/structs3.rs
@@ -3,7 +3,6 @@
 // exercise we have defined the Package struct and we want to test some logic attached to it,
 // make the code compile and the tests pass! If you have issues execute `rustlings hint structs3`
 
-// I AM NOT DONE
 
 #[derive(Debug)]
 struct Package {
@@ -15,18 +14,18 @@ struct Package {
 impl Package {
     fn new(sender_country: String, recipient_country: String, weight_in_grams: i32) -> Package {
         if weight_in_grams <= 0 {
-            // Something goes here...
+            panic!("Package can't have no/negative weight!")
         } else {
             return Package {sender_country, recipient_country, weight_in_grams};
         }
     }
 
-    fn is_international(&self) -> ??? {
-        // Something goes here...
+    fn is_international(&self) -> bool {
+        self.sender_country != self.recipient_country
     }
 
-    fn get_fees(&self, cents_per_kg: i32) -> ??? {
-        // Something goes here... (beware of grams to kg conversion)
+    fn get_fees(&self, cents_per_kg: i32) -> i32 {
+        self.weight_in_grams * cents_per_kg / 1000 as i32
     }
 }
 
@@ -47,7 +46,7 @@ mod tests {
     fn create_international_package() {
         let sender_country = String::from("Spain");
         let recipient_country = String::from("Russia");
-        
+
         let package = Package::new(sender_country, recipient_country, 1200);
 
         assert!(package.is_international());
@@ -58,7 +57,7 @@ mod tests {
         let sender_country = String::from("Spain");
         let recipient_country = String::from("Spain");
 
-        let cents_per_kg = ???;
+        let cents_per_kg = 3000;
         
         let package = Package::new(sender_country, recipient_country, 1500);
         

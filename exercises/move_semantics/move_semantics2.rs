66951b3cf023a257712e2e9d0389d750988e1408
kazesberger <klaus.azesberger@bearingpoint.com>
2020-10-02 11:39:27 +0200

    Add day 5

--- exercises/move_semantics/move_semantics2.rs
+++ exercises/move_semantics/move_semantics2.rs
@@ -2,22 +2,57 @@
 // Make me compile without changing line 13!
 // Execute `rustlings hint move_semantics2` for hints :)
 
-// I AM NOT DONE
 
-fn main() {
-    let vec0 = Vec::new();
+fn solution1 () {
+    let mut vec0 = Vec::new();
 
-    let mut vec1 = fill_vec(vec0);
+    let mut vec1 = fill_vec1(vec0.clone());
 
     // Do not change the following line!
     println!("{} has length {} content `{:?}`", "vec0", vec0.len(), vec0);
 
     vec1.push(88);
 
+    // println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
     println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
 }
 
-fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
+fn solution2() {
+    let mut vec0 = Vec::new();
+
+    let mut vec1 = fill_vec2(&vec0);
+
+    // Do not change the following line!
+    println!("{} has length {} content `{:?}`", "vec0", vec0.len(), vec0);
+
+    vec1.push(88);
+
+    // println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
+    println!("{} has length {} content `{:?}`", "vec1", vec1.len(), vec1);
+
+}
+
+fn solution3 () {
+    let mut vec0 = Vec::new();
+
+    fill_vec3(&mut vec0);
+
+    // Do not change the following line!
+    println!("{} has length {} content `{:?}`", "vec0", vec0.len(), vec0);
+
+    vec0.push(88);
+
+    println!("{} has length {} content `{:?}`", "vec0", vec0.len(), vec0);
+}
+
+
+fn main() {
+    solution1();
+    solution2();
+    solution3();
+}
+
+fn fill_vec1(vec: Vec<i32>) -> Vec<i32> {
     let mut vec = vec;
 
     vec.push(22);
@@ -25,4 +60,19 @@ fn fill_vec(vec: Vec<i32>) -> Vec<i32> {
     vec.push(66);
 
     vec
+}
+fn fill_vec2(vec: &Vec<i32>) -> Vec<i32> {
+    let mut vec = vec.clone();
+
+    vec.push(22);
+    vec.push(44);
+    vec.push(66);
+
+    vec
+}
+
+fn fill_vec3(vec: &mut Vec<i32>) {
+    vec.push(22);
+    vec.push(44);
+    vec.push(66);
 }
